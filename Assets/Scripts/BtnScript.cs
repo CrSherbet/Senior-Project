77fcad41486b5422477d565d1cedbbdf8ca72472
warisara <warisaracr.y@gmail.com>
2018-12-26 10:53:18 +0700

    init project

---
+++ Assets/Scripts/BtnScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BtnScript : MonoBehaviour {
+
+    void Start() { 
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void LoadScene(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+}
