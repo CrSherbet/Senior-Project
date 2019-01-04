a3b8b2406f4c122bb1accb1dff13caf894068b84
warisara <warisaracr.y@gmail.com>
2019-01-04 13:47:19 +0700

    sync and show popup

---
+++ Assets/Scripts/AccessMain.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class AccessMain : MonoBehaviour {
+
+    private float timer = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if(timer > 2.0f) {
+            AccessMainPage();
+        }      
+	}
+
+    public void AccessMainPage () {
+        SceneManager.LoadScene("MainScene");
+    }
+    
+}
