f72a395f344ccd73ea2aeb90a40b6f4a96369135
warisara <warisaracr.y@gmail.com>
2019-03-24 21:22:18 +0700

    add 3d objects to show

--- Assets/Scripts/ModelController.cs
+++ Assets/Scripts/ModelController.cs
@@ -5,53 +5,64 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class ModelController : MonoBehaviour {
 
-	private BoxCollider2D collider;
+		private BoxCollider2D collider;
+		Transform RiceModel;
+		Transform HouseModel;
+		Transform PondModel;
+		Transform TreeModel;
 
-	void Start () {
-		LineRenderer lineRenderer = GetComponent<LineRenderer>();
-		Vector3[] linePos = Controller.currModel.linePos;
-		
-		Vector3[] area = Controller.currModel.area;
-		
-		var result = new List<Vector3>();
-		result.AddRange(linePos);
-		
-		if (area != null) {
-			result.AddRange(area);
-		} 
+		void Start () {
+				RiceModel = GameObject.Find("RiceModel").GetComponent<Transform>();
+				PondModel = GameObject.Find("PondModel").GetComponent<Transform>();
+				HouseModel = GameObject.Find("HouseModel").GetComponent<Transform>();
+				TreeModel = GameObject.Find("TreeModel").GetComponent<Transform>();
+	
+				LineRenderer lineRenderer = GetComponent<LineRenderer>();
+				Vector3[] linePos = Controller.currModel.linePos;
+				
+				Vector3[] area = Controller.currModel.area;
+				
+				List<Vector3> result = new List<Vector3>();
+				result.AddRange(linePos);
+				
+				if (area != null) {
+					result.AddRange(area);
+				} 
 
-		Vector3[] showResult = result.ToArray();
+				Vector3[] showResult = result.ToArray();
 
-		lineRenderer.positionCount = showResult.Length;
+				lineRenderer.positionCount = showResult.Length;
+				
+				for (int i = 0; i < showResult.Length; i++) {
+						lineRenderer.SetPosition(i, showResult[i]);
+				}
+
+		}
 		
-		for (int i = 0; i < showResult.Length; i++) {
-				lineRenderer.SetPosition(i, showResult[i]);
+		// Update is called once per frame
+		void Update () {
+				// RiceModel.position = new Vector3();
+				// HouseModel.position = new Vector3();
+				// PondModel.position = new Vector3();
+				// TreeModel.position = new Vector3();
 		}
 
-	}
+		//Random point in area [Random X and Y]
+		public Vector3 PointInArea() {
+				collider = GetComponent<BoxCollider2D>();
+				var bounds = collider.bounds;
+				var center = bounds.center;
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+				float x = 0;
+				float y = 0;
+				do {
+						x = UnityEngine.Random.Range(center.x - bounds.extents.x, center.x + bounds.extents.x);
+						y = UnityEngine.Random.Range(center.y - bounds.extents.y, center.y + bounds.extents.y);
+				} while (Physics2D.OverlapPoint(new Vector2(x, y), LayerMask.NameToLayer("Area")) == null);
 
-	//Random point in area [Random X and Y]
-	public Vector3 PointInArea() {
-		collider = GetComponent<BoxCollider2D>();
-        var bounds = collider.bounds;
-        var center = bounds.center;
- 
-        float x = 0;
-        float y = 0;
-        do {
-            x = UnityEngine.Random.Range(center.x - bounds.extents.x, center.x + bounds.extents.x);
-            y = UnityEngine.Random.Range(center.y - bounds.extents.y, center.y + bounds.extents.y);
-        } while (Physics2D.OverlapPoint(new Vector2(x, y), LayerMask.NameToLayer("Area")) == null);
-
-		Debug.Log("X" + x);
-		Debug.Log("Y" + y);
- 
-        return new Vector3(x, y, 0);
-    }
-
+				Debug.Log("X" + x);
+				Debug.Log("Y" + y);
+	
+				return new Vector3(x, y, 0);
+		}
 }
